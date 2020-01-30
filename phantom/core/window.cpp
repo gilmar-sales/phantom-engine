c8fcec96d0c5ef0717e605af622b1784ed3e3370
gilmarxd <gilmar_custodio_@hotmail.com>
2020-01-29 23:31:10 -0300

    create glfw and swapbuffers

--- phantom/core/window.cpp
+++ phantom/core/window.cpp
@@ -10,8 +10,16 @@ namespace ph {
             std::cout << "Couldn't initialize GLFW!" << std::endl;
         }
 
+        m_window = glfwCreateWindow(data.width, data.height, data.title.c_str());
+
+        glfwMakeContextCurrent(m_window);
+        
         if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
             std::cout << "Couldn't initialize GLAD!" << std::endl;
         }
+    }
+
+    void Window::on_update() {
+        glfwSwapBuffers(m_window);
     }
 }
