fdad9a5055c3f6b0b303217d19d37c6cc98a9fd0
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-22 17:50:37 -0300

    fix include

--- phantom/src/core/log.cpp
+++ phantom/src/core/log.cpp
@@ -1,6 +1,8 @@
+#include <phpch.h>
+
 #include "log.h"
 
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/stdout_sinks.h>
 
 namespace ph {
     std::shared_ptr<spdlog::logger> Log::s_core_logger;
@@ -8,10 +10,14 @@ namespace ph {
 
     void Log::init() {
         spdlog::set_pattern("%^[%c] [%n] [%l]%$ %v");
+
+
 		s_core_logger = spdlog::stdout_color_mt("phantom");
 		s_core_logger->set_level(spdlog::level::trace);
+        PH_CORE_INFO("Initialize logger!");
 
 		s_client_logger = spdlog::stdout_color_mt("app");
 		s_client_logger->set_level(spdlog::level::trace);
+        PH_CLIENT_INFO("Initialize logger!");
     }
 }
