f07de016aed281ba1cacac76bef1382e6f8cdca2
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-25 09:09:40 -0300

    add OpenGL FrameBuffer Impl

---
+++ phantom/src/platform/opengl/opengl_frame_buffer.cpp
@@ -0,0 +1,84 @@
+#include <phpch.h>
+#include "opengl_frame_buffer.h"
+
+#include <glad/glad.h>
+
+namespace ph {
+
+	static const uint32_t s_max_framebuffer_size = 8192;
+
+	OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& spec)
+		: m_specification(spec)
+	{
+		invalidate();
+	}
+
+	OpenGLFramebuffer::~OpenGLFramebuffer()
+	{
+		glDeleteFramebuffers(1, &m_renderer_id);
+		glDeleteTextures(1, &m_color_attachment);
+		glDeleteTextures(1, &m_depth_attachment);
+	}
+
+	void OpenGLFramebuffer::invalidate()
+	{
+		if (m_renderer_id)
+		{
+			glDeleteFramebuffers(1, &m_renderer_id);
+			glDeleteTextures(1, &m_color_attachment);
+			glDeleteTextures(1, &m_depth_attachment);
+		}
+
+		glCreateFramebuffers(1, &m_renderer_id);
+		glBindFramebuffer(GL_FRAMEBUFFER, m_renderer_id);
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_color_attachment);
+		glBindTexture(GL_TEXTURE_2D, m_color_attachment);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_specification.width, m_specification.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_color_attachment, 0);
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_depth_attachment);
+		glBindTexture(GL_TEXTURE_2D, m_depth_attachment);
+		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_specification.width, m_specification.height);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0);
+
+		if (!(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE))
+			PH_CORE_ERROR( "Framebuffer is incomplete!");
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void OpenGLFramebuffer::bind()
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, m_renderer_id);
+		glViewport(0, 0, m_specification.width, m_specification.height);
+	}
+
+	void OpenGLFramebuffer::unbind()
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void OpenGLFramebuffer::resize(uint32_t width, uint32_t height)
+	{
+		if (width == m_specification.width && height == m_specification.height)
+		{
+			PH_CORE_WARN("Attempted to rezize framebuffer with same dimensions");
+			return;
+		}
+
+		if (width == 0 || height == 0 || width > s_max_framebuffer_size || height > s_max_framebuffer_size)
+		{
+			PH_CORE_WARN("Attempted to rezize framebuffer to {0}, {1}", width, height);
+			return;
+		}
+		m_specification.width = width;
+		m_specification.height = height;
+
+		invalidate();
+	}
+
+}
---
+++ phantom/src/platform/opengl/opengl_frame_buffer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <renderer/frame_buffer.h>
+
+namespace ph {
+
+	class OpenGLFramebuffer : public Framebuffer
+	{
+	public:
+		OpenGLFramebuffer(const FramebufferSpecification& spec);
+		virtual ~OpenGLFramebuffer();
+
+		void invalidate();
+
+		virtual void bind() override;
+		virtual void unbind() override;
+
+		virtual void resize(uint32_t width, uint32_t height) override;
+
+		virtual uint32_t get_color_attachment_renderer_id() const override { return m_color_attachment; }
+
+		virtual const FramebufferSpecification& get_specification() const override { return m_specification; }
+	private:
+		uint32_t m_renderer_id = 0;
+		uint32_t m_color_attachment = 0;
+		uint32_t m_depth_attachment = 0;
+		FramebufferSpecification m_specification;
+	};
+
+}
